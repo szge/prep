7a9ebc5d676e08e13a8451bc5714fb0e0872658f
Jonathan Chiang <jchiangconan@gmail.com>
2025-04-01 23:51:45 -0400

    first problem

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # prep
 i learn to code in a way where my thought process can be inspected
+
+## 20250401
+day 1, spent hours figuring out how to get c++ intellisense to work on my mac but it wasn't cooperating.
+at least for now I'll probably won't run my solutions here.
+
+I learned that two pointers are potentially useful when searching for a pair of elements in an array.
---
+++ neetcode/max_water_container.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    public:
+        // int maxArea(vector<int>& heights) {
+        //     // brute force time o(n^2)
+        //     int maxWater = 0;
+        //     int n = heights.size();
+        //     for (int i = 0; i < n; i++) {
+        //         for (int j = i + 1; j < n; j++) {
+        //             int width = j - i;
+        //             int height = min(heights[i], heights[j]);
+        //             int area = width * height;
+        //             maxWater = max(maxWater, area);
+        //         }
+        //     }
+        //     return maxWater;
+        // }
+    
+        // things I know:
+        // width = j - i
+        // height = min(heights[i], heights[j])
+        // area = width * height
+    
+        // idea 1: "pouring water" from the top??
+        // idea 2: could iterate over the widths (container widths = 1, 2, ...)
+        // this would be o(n^2) but maybe there's a clever way to do it
+        // idea 3: two pointer alg... but how to choose which side to update?
+    
+        int maxArea(vector<int>& heights) {
+            int l = 0;
+            int r = heights.size() - 1;
+            int maxArea = 0;
+    
+            while (l < r) {
+                int width = r - l;
+                int height = min(heights[l], heights[r]);
+                int area = width * height;
+                maxArea = max(maxArea, area);
+    
+                if (heights[l] <= heights[r]) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+    
+            return maxArea;
+        }
+    };
+    
