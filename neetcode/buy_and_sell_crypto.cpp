bddcfd67f8dfbc81ae4543a35822da20ecc34eb4
Jonathan Chiang <jchiangconan@gmail.com>
2025-04-04 00:17:33 -0400

    20250403

---
+++ neetcode/buy_and_sell_crypto.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+        // int maxProfit(vector<int>& prices) {
+        //     vector<int> min_buy(prices.size());
+        //     int curr_min = prices[0];
+        //     for (int i = 0; i < prices.size(); i++) {
+        //         curr_min = min(curr_min, prices[i]);
+        //         min_buy[i] = curr_min;
+        //     }
+        //     int max_profit = 0;
+        //     for (int i = 0; i < prices.size(); i++) {
+        //         int curr_profit = prices[i] - min_buy[i]; // sell now price
+        //         max_profit = max(max_profit, curr_profit);
+        //     }
+        //     return max_profit;
+        // }
+    
+        // one loop
+        int maxProfit(vector<int>& prices) {
+            int max_profit = 0;
+            int min_buy = prices[0];
+    
+            for (int i = 0; i < prices.size(); i++) {
+                int sell = prices[i];
+                int curr_profit = sell - min_buy;
+                max_profit = max(max_profit, curr_profit);
+                min_buy = min(min_buy, sell);
+            }
+            return max_profit;
+        }
+    
+    };
+    
---
+++ neetcode/longest_substring_without_duplicates.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    public:
+        // first attempt
+        // time o(n^2) due to substring creation and find
+        // int lengthOfLongestSubstring(string s) {
+        //     int l = 0;
+        //     int max_len = 0;
+        //     for (int r = 0; r < s.size(); r++) {
+        //         int len = r - l + 1;
+        //         // create a string from l to r, but exclude s[r]
+        //         string substring = s.substr(l, len - 1);
+        //         std::size_t found = substring.find(s[r]);
+        //         if (found == std::string::npos) {
+        //             // there's no duplicate, so we can include s[r]
+        //             max_len = max(max_len, len);
+        //         } else {
+        //             // there's a duplicate
+        //             // remove everything up to and including the first instance of s[r]
+        //             l += found + 1;
+        //         }
+        //     }
+        //     return max_len;
+        // }
+    
+        // second attempt
+        // basically my previous solution
+        // but instead of using an actual substring
+        // I can use a set to represent it
+        // so I can check for s[r] in o(1)
+        int lengthOfLongestSubstring(string s) {
+            int l = 0;
+            int max_len = 0;
+            unordered_set<char> curr_substr_set;
+    
+            for (int r = 0; r < s.size(); r++) {
+                // remove all letters up until s[r] from the substring
+                while (curr_substr_set.count(s[r]) > 0) {
+                    curr_substr_set.erase(s[l]);
+                    l++;
+                }
+                curr_substr_set.insert(s[r]);
+                int substr_len = r - l + 1;
+                max_len = max(max_len, substr_len);
+            }
+            return max_len;
+        }
+    };
+    
